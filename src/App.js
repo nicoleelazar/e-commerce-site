75ca388879ee203d063b044beed0d2135a657b93
Nicole Elazar <nicole.elazar@gmail.com>
2020-03-26 19:52:24 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+     <div className="row">
+      <div className="col-6">first column</div>
+      <div className="col-6">
+        <span>
+          <i className="fas fa-home" />
+          </span>
+          </div>
+
+     </div>
+      
+    </div>
+
+
+  );
+}
+
+export default App;
